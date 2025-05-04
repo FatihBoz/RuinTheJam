0d55c556c5c4e11cf390679f179c20ba16082dc8
Mustafa Cetin <115427747+mustafa-cetin@users.noreply.github.com>
2025-05-04 16:37:53 +0300

    SoundManager

---
+++ Assets/Components/SoundManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager Instance;
+    private AudioSource audioSource;
+
+
+    [SerializeField]
+    private AudioClip hitClick;
+    [SerializeField]
+    private AudioClip uiClick;
+    private void Awake()
+    {
+        Instance = this;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayHitClick()
+    {
+        audioSource.PlayOneShot(hitClick);
+    }
+    public void PlayUIClick()
+    {
+        audioSource.PlayOneShot(uiClick);
+    }
+
+
+}
